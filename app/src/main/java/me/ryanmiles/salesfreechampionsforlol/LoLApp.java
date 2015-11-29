d103a319b3977f5c2683eab6bac95c53792b169f
Ryanm14 <ryanm1114@gmail.com>
2015-11-28 21:58:03 -0500

    Setup Api Key and sample call

---
+++ app/src/main/java/me/ryanmiles/salesfreechampionsforlol/LoLApp.java
@@ -0,0 +1,41 @@
+package me.ryanmiles.salesfreechampionsforlol;
+
+import android.app.Application;
+import android.os.StrictMode;
+
+import com.robrua.orianna.api.core.RiotAPI;
+import com.robrua.orianna.type.core.common.Region;
+
+import timber.log.Timber;
+
+/**
+ * Created by Ryanm14 on 11/28/2015.
+ */
+public final class LoLApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .penaltyFlashScreen()
+                    .build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build());
+
+            Timber.d("Strict mode and Timber initialized");
+        } else {
+            // TODO Crashlytics.start(this);
+            // TODO Timber.plant(new CrashlyticsTree());
+        }
+        // TODO: 11/28/2015 Change and save Regions and Strict Mode
+        RiotAPI.setRegion(Region.NA);
+        RiotAPI.setAPIKey(Constants.RIOT_API);
+    }
+}
--- app/src/main/java/me/ryanmiles/salesfreechampionsforlol/MainActivity.java
+++ app/src/main/java/me/ryanmiles/salesfreechampionsforlol/MainActivity.java
@@ -3,11 +3,34 @@ package me.ryanmiles.salesfreechampionsforlol;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.robrua.orianna.api.core.AsyncRiotAPI;
+import com.robrua.orianna.type.api.Action;
+import com.robrua.orianna.type.core.summoner.Summoner;
+import com.robrua.orianna.type.exception.APIException;
+
+import timber.log.Timber;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        AsyncRiotAPI.getSummonerByName(new Action<Summoner>() {
+            @Override
+            public void handle(APIException exception) {
+
+            }
+
+            @Override
+            public void perform(Summoner responseData) {
+                Timber.d(responseData.getRevisionDate() + "");
+            }
+        }, "OJjuiceman");
+
+
+
     }
 }
